c26a8280be854841c1a5f708f9ff821e7212a7c9
tangym <tangym.edwin@gmail.com>
2015-10-30 15:18:40 +0000

    update: scipy

--- dev-env-conf.sh
+++ dev-env-conf.sh
@@ -1,10 +1,10 @@
 apt-get update
 apt-get install -y \
     tmux nano unzip \
-    python3 python3-pip \
+    python3 python3-pip python3-scipy \
     nginx \
     texlive-xetex
 pip3 install flask flask_restful \
-     numpy scipy \
+     numpy \
      pyyaml markdown 
 pip3 install pandas pylab
