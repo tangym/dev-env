8e10394dea4093668d2f230040a50847049e0781
tangym <tangym.edwin@gmail.com>
2015-10-30 08:25:40 +0000

    init

---
+++ dev-env-conf.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get install -y \
+    tmux nano \
+    python3 python3-pip \
+    nginx \
+    texlive-xetex
+pip3 install flask flask_restful \
+     numpy pylab pandas \
+     pyyaml markdown 
